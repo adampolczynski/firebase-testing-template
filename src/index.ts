a5b87012f39284f8d04d1d5a809dbea0a38aa240
AdamPolczynski <adam_polczynski@yahoo.com>
2024-10-11 16:22:18 +0200

    configured build and initial cloud function with express

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,33 @@
-import { initializeApp as firebaseApp } from 'firebase/app';
-import { initializeApp as firebaseAdminApp } from 'firebase-admin';
-// import { parse as parseEnv, config as envConfig } from 'dotenv';
+import { initializeApp as firebaseApp, FirebaseOptions } from 'firebase/app';
+import * as fbAdmin from 'firebase-admin';
 import { getFirestore } from 'firebase/firestore';
-// import { api } from './functions';
+import { api } from './functions';
 
-// const env = envConfig({ path: '../.env' }).parsed;
-// console.debug('env', env);
-// if (!env) {
-//   process.exit(1);
-// }
+import { config } from '@dotenvx/dotenvx';
+config();
+
+// const { defineSecret } = require('firebase-functions/params');
+// const apiKey = defineSecret('FB_API_KEY');
+//  safe place to access a parameter's value.
+// const { onInit } = require('firebase-functions/v2/core');
+
 const env = process.env;
 
-const firebaseConfig = {
-  apiKey: env.FIREBASE_API_KEY,
-  authDomain: env.FIREBASE_AUTH_DOMAIN,
-  projectId: env.FIREBASE_PROJECT_ID,
-  storageBucket: env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: env.FIREBASE_APP_ID,
+if (!env) {
+  process.exit(1);
+}
+
+const firebaseConfig: FirebaseOptions = {
+  apiKey: env.FB_API_KEY,
+  authDomain: env.FB_AUTH_DOMAIN,
+  projectId: env.FB_PROJECT_ID,
+  storageBucket: env.FB_STORAGE_BUCKET,
+  messagingSenderId: env.FB_MESSAGING_SENDER_ID,
+  appId: env.FB_APP_ID,
 };
 
 const app = firebaseApp(firebaseConfig);
-const adminApp = firebaseAdminApp(firebaseConfig);
+const adminApp = fbAdmin.initializeApp({ projectId: env.FB_PROJECT_ID });
 const db = getFirestore(app);
+
+export default { api };
