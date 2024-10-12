5d147fd5fe93c00eac7c3505055837cf5367cfd9
AdamPolczynski <adam_polczynski@yahoo.com>
2024-10-12 19:26:28 +0200

    tried easy-busboy within cloud function

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,10 +1,12 @@
+import { easyBusboy } from 'easy-busboy';
 import express from 'express';
 import { onRequest } from 'firebase-functions/v2/https';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  response.send('Hello from Firebase cloud function!');
+app.post('/', async (request, response) => {
+  const { fields, files } = await easyBusboy(request);
+  response.send({ fields, files });
 });
 
 export const api = onRequest(app);
